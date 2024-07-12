10abf430fb815f68280f463c876f08c61167e7f2
Brayan Castillejo Florez <brayan03sena2019@gmail.com>
2024-07-12 11:56:48 -0500

    Fit add routing
    React Router dependency added and minor styles improvement to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ import UserIcon from "./icons/UserIcon";
 
 export default function Header() {
   return (
-    <header className="bg-primary flex">
+    <header className="bg-primary flex h-16 w-full gap-4 p-4">
       <HamburgerIcon />
-      <h2>Electrotech</h2>
+      <h2 className="mr-auto">Electrotech</h2>
       <SearchIcon />
       <UserIcon />
     </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import Login from "./components/Login.tsx";
+import Register from "./components/Register.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
